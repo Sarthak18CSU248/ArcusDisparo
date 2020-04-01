993e1400c9b9496fbadc062d313ad080e40f9893
Sarthak18CSU248 <57385445+Sarthak18CSU248@users.noreply.github.com>
2020-04-01 21:12:18 +0530

    Developer Build(IOS)

--- Assets/Script/AimCamera.cs
+++ Assets/Script/AimCamera.cs
@@ -1,22 +1,33 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class AimCamera : MonoBehaviour {
     private Rigidbody rb;
     private bool inAir = false;
     private bool crouch = false;
     private Vector3 ogScale;    
-    [SerializeField] float verSensitivity = 2;
-    [SerializeField] float horSensitivity = 2;
+    [SerializeField] float verSensitivity = 0.5f;
+    [SerializeField] float horSensitivity = 0.5f;
     [SerializeField] float speed = 0.5f;
     [SerializeField] float jumpForce = 1f;
     [SerializeField] float crouchScale = 1.5f;
     [SerializeField] Camera cam = null;
+
+    private Vector3 firstpoint;
+    private Vector3 secondpoint;
+    private float yAngle;
+    private float xAngle;
+    private float yAngleTemp;
+    private float xAngleTemp;
+
+    protected Joystick joystick;
 
     //Called when the gameobject is created (which is at the start of the game)
     private void Start() {
         rb = GetComponent<Rigidbody>(); //get the player's rigidbody component
         Cursor.visible = false; //make it so the cursor isn't visible
         ogScale = transform.localScale;
+        joystick = FindObjectOfType<Joystick>();
     }
 
     //called once per frame
@@ -58,14 +69,32 @@ public class AimCamera : MonoBehaviour {
     }
 
     //When the player attempts to move their camera
-    private void CheckForCameraMovement() {
-        float mouseX = Input.GetAxisRaw("Mouse X"); //get x input
-        float mouseY = Input.GetAxisRaw("Mouse Y"); //get y input
+    private void CheckForCameraMovement()
+    {
+        /*if(Input.touchCount > 0)
+        {
+            if(Input.GetTouch(0).phase ==TouchPhase.Began)
+            {
+                firstpoint = Input.GetTouch(0).position;
+            }
 
-        Vector3 rotateX = new Vector3(mouseY * verSensitivity, 0, 0); //calculate the x rotation based on the y input
-        Vector3 rotateY = new Vector3(0, mouseX * horSensitivity, 0); //calculate the y rotation based on the x input
+            if(Input.GetTouch(0).phase == TouchPhase.Moved)
+            {
+                secondpoint = Input.GetTouch(0).position;
+                xAngle = (float)(xAngleTemp + (secondpoint.x - firstpoint.x) * 180.0 / Screen.width);
+                yAngle = (float)(yAngleTemp - (secondpoint.y - firstpoint.y) * 90.0 / Screen.height);
+                this.transform.rotation = Quaternion.Euler(yAngle, xAngle, 0);
+            }
+        }*/
+        //float mouseX = Input.GetAxisRaw("Mouse X"); //get x input joystick.Horizontal;
+        //float mouseY = Input.GetAxisRaw("Mouse Y"); //get y input joystick.Vertical;
+        Vector3 dir = Vector3.zero;
+        dir.x = joystick.Horizontal;
+        dir.z = joystick.Vertical;
+        Vector3 rotateX = new Vector3(dir.z * verSensitivity, dir.x*horSensitivity, 0); //calculate the x rotation based on the y input
+        Vector3 rotateY = new Vector3(0, dir.x * horSensitivity, 0); //calculate the y rotation based on the x input
+        //rb.MoveRotation(rb.rotation * Quaternion.Euler(rotateY)); //rotate rigid body
+        transform.Rotate(-rotateX); //rotate the camera negative to the x rotation (so the movement isn't inversed)
 
-        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotateY)); //rotate rigid body
-        cam.transform.Rotate(-rotateX); //rotate the camera negative to the x rotation (so the movement isn't inversed)
     }
 }
--- Assets/Script/ShootBow.cs
+++ Assets/Script/ShootBow.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class ShootBow : MonoBehaviour {
 
